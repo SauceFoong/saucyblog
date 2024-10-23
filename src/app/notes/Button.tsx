0102bb77c28faed08fb83b474c660946d08e3d15
saucefoong <saucefoong@gmail.com>
2024-10-23 08:56:46 +0800

    Removed unused code

--- src/app/notes/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
-import { useFormStatus } from "react-dom";
-
-export default function ButtonComponent() {
-  const { pending } = useFormStatus();
-  return <Button type="submit">{pending ? "Adding..." : "Add"}</Button>;
-}
